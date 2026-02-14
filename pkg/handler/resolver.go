e52a83d4327230cfc6d0b76a5b6cce22aea5ddf1
SohamRatnaparkhi <soham.ratnaparkhi@gmail.com>
2026-02-14 23:47:35 +0530

    refactor: rename handlerresolvers import alias to handlerResolvers for consistency

--- pkg/handler/resolver.go
+++ pkg/handler/resolver.go
@@ -3,40 +3,40 @@ package handler
 import (
 	"reflect"
 
-	handlerresolvers "github.com/sohamratnaparkhi/go-fast/pkg/handler/resolvers"
+	handlerResolvers "github.com/sohamratnaparkhi/go-fast/pkg/handler/resolvers"
 )
 
-type FieldIndexProvider = handlerresolvers.FieldIndexProvider
-type FieldValueResolver = handlerresolvers.FieldValueResolver
-type FieldResolver = handlerresolvers.FieldResolver
+type FieldIndexProvider = handlerResolvers.FieldIndexProvider
+type FieldValueResolver = handlerResolvers.FieldValueResolver
+type FieldResolver = handlerResolvers.FieldResolver
 
-type BodyResolver = handlerresolvers.BodyResolver
-type HeaderResolver = handlerresolvers.HeaderResolver
-type QueryResolver = handlerresolvers.QueryResolver
-type PathVarResolver = handlerresolvers.PathVarResolver
-type CookieResolver = handlerresolvers.CookieResolver
+type BodyResolver = handlerResolvers.BodyResolver
+type HeaderResolver = handlerResolvers.HeaderResolver
+type QueryResolver = handlerResolvers.QueryResolver
+type PathVarResolver = handlerResolvers.PathVarResolver
+type CookieResolver = handlerResolvers.CookieResolver
 
 // NewBodyResolver constructs a resolver for json:"body" fields.
 func NewBodyResolver(fieldIdx int, fieldType reflect.Type) *BodyResolver {
-	return handlerresolvers.NewBodyResolver(fieldIdx, fieldType)
+	return handlerResolvers.NewBodyResolver(fieldIdx, fieldType)
 }
 
 // NewHeaderResolver constructs a resolver for json:"header:<name>" fields.
 func NewHeaderResolver(fieldIdx int, headerName string, fieldType reflect.Type) *HeaderResolver {
-	return handlerresolvers.NewHeaderResolver(fieldIdx, headerName, fieldType)
+	return handlerResolvers.NewHeaderResolver(fieldIdx, headerName, fieldType)
 }
 
 // NewQueryResolver constructs a resolver for json:"query:<name>" fields.
 func NewQueryResolver(fieldIdx int, queryName string, fieldType reflect.Type) *QueryResolver {
-	return handlerresolvers.NewQueryResolver(fieldIdx, queryName, fieldType)
+	return handlerResolvers.NewQueryResolver(fieldIdx, queryName, fieldType)
 }
 
 // NewPathVarResolver constructs a resolver for json:"path:<name>" fields.
 func NewPathVarResolver(fieldIdx int, paramName string, fieldType reflect.Type) *PathVarResolver {
-	return handlerresolvers.NewPathVarResolver(fieldIdx, paramName, fieldType)
+	return handlerResolvers.NewPathVarResolver(fieldIdx, paramName, fieldType)
 }
 
 // NewCookieResolver constructs a resolver for json:"cookie:<name>" fields.
 func NewCookieResolver(fieldIdx int, cookieName string, fieldType reflect.Type) *CookieResolver {
-	return handlerresolvers.NewCookieResolver(fieldIdx, cookieName, fieldType)
+	return handlerResolvers.NewCookieResolver(fieldIdx, cookieName, fieldType)
 }
